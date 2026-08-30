818ddb01c826fab6375df8a1a4b1634d09138285
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-08-30 19:53:17 +0300

    0

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
 
 app.use(express.static(__dirname));
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
+
